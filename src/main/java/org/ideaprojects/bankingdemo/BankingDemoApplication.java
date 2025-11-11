cc8dd4352cdb14a8fdf51822c7fa862e02dae954
ulvihuseynov <ulvihuseynov441@gmail.com>
2025-11-12 02:18:43 +0400

    Initial commit

---
+++ src/main/java/org/ideaprojects/bankingdemo/BankingDemoApplication.java
@@ -0,0 +1,13 @@
+package org.ideaprojects.bankingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ideaprojects/bankingdemo/BankingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ideaprojects.bankingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
